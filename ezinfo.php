8b229c41dda616ee6a5a6ef1a16b31f341f45aa8
7x <info@se7enx.com>
2024-10-06 01:40:57 +0000

    Added: Added LICENSE files and ezinfo file and composer package configuration file

---
+++ ezinfo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File containing the owmultisite copyright information file
+ *
+ * @copyright Copyright (C) 1999 - 2024 7x. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
+ * @version //autogentag//
+ * @package owmultisite
+ */
+
+class owmultisiteInfo
+{
+    public static function info()
+    {
+        return array( 'Name' => "<a href='https://github.com/se7enxweb/owmultisite'>OW Multi Site</a>",
+                      'Version' => "1.4.0",
+                      'Copyright' => "Copyright (C) 1999 - 2024 <a href='https://se7enx.com' title='7x'>7x</a> and 2012 Open Wide",
+                      'License' => "GNU General Public License v3.0",
+                      'info_url' => "https://github.com/se7enxweb/owmultisite"
+                    );
+    }
+}
+
+?>
